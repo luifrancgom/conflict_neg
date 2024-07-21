4c5b831008a5c8ae60de722e9d638dded87c0e7c
luifrancgom <luifrancgom@gmail.com>
2024-07-21 03:40:58 -0500

    Adding logos, template and updating syllabus 2024

--- 000_script_simulation.R
+++ 000_script_simulation.R
@@ -1,17 +1,16 @@
+# Libraries ----
 library(tidyverse)
 library(tidymodels)
 
-# Within groups
-tibble(role = c("A", "B", "C")) %>% 
-  rep_sample_n(size = 3, 
-               replace = FALSE, 
-               reps = 2)
+# Within groups ----
+tibble(role = c("A", "B", "C")) |> 
+    rep_sample_n(size = 3, 
+                 replace = FALSE, 
+                 reps = 2)
 
-# Between groups
-tibble(role = c("A", "B", "C")) %>% 
+# Between groups ----
+tibble(role = c("A", "B", "C")) |> 
   rep_sample_n(size = 3, 
                replace = FALSE, 
-               reps = 5) %>%
-  ungroup() %>%  
-  select(role) %>% 
-  View()
+               reps = 5) |>
+  ungroup()
